78d89f97d5253dfae80397d152176d1aa2a9623d
javieriturra <javier.iturra.b@gmail.com>
2020-03-24 23:55:28 -0300

    to_json

--- src/main/scala/RateToConsoleApp.scala
+++ src/main/scala/RateToConsoleApp.scala
@@ -1,5 +1,4 @@
-import org.apache.log4j.{Level, Logger}
-import org.apache.spark.sql.functions.expr
+import org.apache.spark.sql.functions.{expr, struct, col, to_json}
 import org.apache.spark.sql.streaming.{OutputMode, Trigger}
 import org.apache.spark.sql.{DataFrame, SparkSession}
 
@@ -7,26 +6,20 @@ object RateToConsoleApp {
 
   val spark: SparkSession = SparkSession.builder.master("local[*]").getOrCreate()
 
-  val streamDf: DataFrame = spark.readStream.format("rate").option("rowsPerSecond", 1).load()
+  final val streamDf: DataFrame = spark.readStream.format("rate").option("rowsPerSecond", 1).load()
 
-  val eventsDf: DataFrame = streamDf.withColumn("key", expr("uuid()"))
+  val eventsDf: DataFrame = streamDf
+    .withColumn("key", expr("uuid()"))
+    .withColumn("value", to_json(struct(col("value"), col("timestamp"))))
+    .select("key", "value")
 
   def load(): Unit = {
-
-    eventsDf.writeStream
+    eventsDf.writeStream // Only 1 stream is allowed
       .format("console")
       .option("truncate", "false")
       .outputMode(OutputMode.Update())
       .trigger(Trigger.ProcessingTime(5 * 1000))
       .start()
-
-    eventsDf.writeStream
-      .format("console")
-      .option("truncate", "false")
-      .outputMode(OutputMode.Update())
-      .trigger(Trigger.ProcessingTime(5 * 1000))
-      .start()
-
   }
 
   def main(args: Array[String]): Unit = {
